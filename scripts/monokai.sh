8723da48fea08854bb98935c5180a7fa6a55c060
Brikkel <rikvanbrakel24@gmail.com>
2024-04-19 17:13:15 +0200

    changed left edge

--- scripts/monokai.sh
+++ scripts/monokai.sh
@@ -2,11 +2,10 @@
 # setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $current_dir/utils.sh
 
-main()
-{
+main() {
   datafile=/tmp/.monokai-tmux-data
 
   # set configuration option variables
@@ -25,9 +24,9 @@ main()
   show_day_month=$(get_tmux_option "@monokai-day-month" false)
   show_refresh=$(get_tmux_option "@monokai-refresh-rate" 5)
   show_kubernetes_context_label=$(get_tmux_option "@monokai-kubernetes-context-label" "")
-  IFS=' ' read -r -a plugins <<< $(get_tmux_option "@monokai-plugins" "network-ping cpu-usage ram-usage")
+  IFS=' ' read -r -a plugins <<<$(get_tmux_option "@monokai-plugins" "network-ping cpu-usage ram-usage")
 
-  # Monokai Pro Color Pallette  
+  # Monokai Pro Color Pallette
   white='#fcfcfa'
   black='#2d2a2e'
   gray='#727072'
@@ -40,14 +39,18 @@ main()
 
   # Handle left icon configuration
   case $show_left_icon in
-    smiley)
-      left_icon="☺";;
-    session)
-      left_icon="#S";;
-    window)
-      left_icon="#W";;
-    *)
-      left_icon=$show_left_icon;;
+  smiley)
+    left_icon="☺"
+    ;;
+  session)
+    left_icon="#S"
+    ;;
+  window)
+    left_icon="#W"
+    ;;
+  *)
+    left_icon=$show_left_icon
+    ;;
   esac
 
   # Handle left icon padding
@@ -70,19 +73,23 @@ main()
 
   # Set timezone unless hidden by configuration
   case $show_timezone in
-    false)
-      timezone="";;
-    true)
-      timezone="#(date +%Z)";;
+  false)
+    timezone=""
+    ;;
+  true)
+    timezone="#(date +%Z)"
+    ;;
   esac
 
   case $show_flags in
-    false)
-      flags=""
-      current_flags="";;
-    true)
-      flags="#{?window_flags,#[fg=${cyan}]#{window_flags},}"
-      current_flags="#{?window_flags,#[fg=${cyan}]#{window_flags},}"
+  false)
+    flags=""
+    current_flags=""
+    ;;
+  true)
+    flags="#{?window_flags,#[fg=${cyan}]#{window_flags},}"
+    current_flags="#{?window_flags,#[fg=${cyan}]#{window_flags},}"
+    ;;
   esac
 
   # sets refresh interval to every 5 seconds
@@ -115,7 +122,7 @@ main()
 
   # Status left
   if $show_powerline; then
-    tmux set-option -g status-left "#[fg=${green},bg=${black}]#{?client_prefix,#[fg=${yellow}],}#[bg=${green},fg=${black},bold]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${green},bg=${gray}]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
+    tmux set-option -g status-left "#[fg=${green},bg=${black}]#{?client_prefix,#[fg=${yellow}],}#[bg=${green},fg=${black},bold]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${green},bg=${gray}]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
     powerbg=${gray}
   else
     tmux set-option -g status-left "#[bg=${green},fg=${black},bold]#{?client_prefix,#[bg=${yellow}],} ${left_icon} "
@@ -127,37 +134,37 @@ main()
   for plugin in "${plugins[@]}"; do
 
     if [ $plugin = "git" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@monokai-git-colors" "green black")
-        script="#($current_dir/git.sh)"     
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-git-colors" "green black")
+      script="#($current_dir/git.sh)"
     fi
 
     if [ $plugin = "battery" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-battery-colors" "red black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-battery-colors" "red black")
       script="#($current_dir/battery.sh)"
     fi
 
     if [ $plugin = "gpu-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-gpu-usage-colors" "red black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-gpu-usage-colors" "red black")
       script="#($current_dir/gpu_usage.sh)"
     fi
 
     if [ $plugin = "cpu-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-cpu-usage-colors" "magenta black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-cpu-usage-colors" "magenta black")
       script="#($current_dir/cpu_info.sh)"
     fi
 
     if [ $plugin = "ram-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-ram-usage-colors" "yellow black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-ram-usage-colors" "yellow black")
       script="#($current_dir/ram_info.sh)"
     fi
 
     if [ $plugin = "network" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-network-colors" "blue black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-network-colors" "blue black")
       script="#($current_dir/network.sh)"
     fi
 
     if [ $plugin = "network-bandwidth" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-network-bandwidth-colors" "blue black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-network-bandwidth-colors" "blue black")
       tmux set-option -g status-right-length 250
       script="#($current_dir/network_bandwidth.sh)"
     fi
@@ -184,13 +191,13 @@ main()
         sleep 0.01
       done
 
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-weather-colors" "magenta black")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-weather-colors" "magenta black")
       script="#(cat $datafile)"
     fi
 
     if [ $plugin = "time" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@monokai-time-colors" "green white")
-      if $show_day_month && $show_military ; then # military time and dd/mm
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@monokai-time-colors" "green white")
+      if $show_day_month && $show_military; then # military time and dd/mm
         script="%a %d/%m %R ${timezone} "
       elif $show_military; then # only military time
         script="%a %m/%d %R ${timezone} "
