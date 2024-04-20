ac3f5a8936d711a15d7bc7de4f68a9f85c60919d
Brikkel <rikvanbrakel24@gmail.com>
2024-04-20 10:23:37 +0200

    yg 4

--- scripts/monokai.sh
+++ scripts/monokai.sh
@@ -123,7 +123,7 @@ main() {
 
   # Status left
   if $show_powerline; then
-    tmux set-option -g status-left "#[fg=${yellow},bg=${background}]#{?client_prefix,#[fg=${green}],}#[bg=${yellow},fg=${background},bold]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${green},bg=${black}]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
+    tmux set-option -g status-left "#[fg=${yellow},bg=${background}]#{?client_prefix,#[fg=${green}],}#[bg=${yellow},fg=${background},bold]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${yellow},bg=${black}]#{?client_prefix,#[fg=${green}],}${left_sep}"
     powerbg=${black}
   else
     tmux set-option -g status-left "#[bg=${green},fg=${background},bold]#{?client_prefix,#[bg=${green}],} ${left_icon} "
